abcb51e5a95bac3db5cdc98bab50a171ee77f7ce
nahidhesenov <hesenovn07@gmail.com>
2024-09-17 16:39:33 +0400

    navbar link update

--- src/Companent/NavBar/ElementLink/index.jsx
+++ src/Companent/NavBar/ElementLink/index.jsx
@@ -2,11 +2,10 @@ import { Link } from 'react-router-dom'
 
 const ElementLink = () => {
   return (
-    <div style={{ display: "flex", justifyContent: 'space-between', width: '40%' }}>
+    <div style={{ display: "flex", justifyContent: 'space-between', width: '30%' }}>
       <Link to={"/productlisting"} style={{fontFamily:'sans-serif', textDecoration:"none" , color:"gray"}}>All products</Link>
       <Link style={{fontFamily:'sans-serif', textDecoration:"none" , color:"gray"}}>About us</Link>
       <Link style={{fontFamily:'sans-serif', textDecoration:"none" , color:"gray"}}>Contact</Link>
-      <Link style={{fontFamily:'sans-serif', textDecoration:"none" , color:"gray"}}>Blog</Link>
       <Link to={"/services"} style={{fontFamily:'sans-serif', textDecoration:"none" , color:"gray"}}>Services</Link>
     </div>
   )
